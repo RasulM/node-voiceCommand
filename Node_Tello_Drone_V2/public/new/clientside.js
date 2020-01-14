d0db33365e8fc0e672379e8b9ac39a0f9653531e
Rasul Mahones <mahonesrasul@gmail.com>
2020-01-13 20:48:05 -0500

    Added  speech reg GUI framework
    Add GUI for speech reg
    add commands for Debug menu

--- Node_Tello_Drone_V2/public/new/clientside.js
+++ Node_Tello_Drone_V2/public/new/clientside.js
@@ -41,5 +41,21 @@ function bop(){
     var strmsg = JSON.stringify(msg)
   //  ws.send(strmsg)
 }
+var voiceCommands ={
+    'hello': function() { alert('Hello world!'); }
+  }
 
+if (annyang) {
+    // Add our commands to annyang
+    annyang.addCommands(voiceCommands);
+  
+    // Tell KITT to use annyang
+    SpeechKITT.annyang();
+  
+    // Define a stylesheet for KITT to use
+    SpeechKITT.setStylesheet('//cdnjs.cloudflare.com/ajax/libs/SpeechKITT/0.3.0/themes/flat.css');
+    SpeechKITT.setInstructionsText("Say a drone command")
+    // Render KITT's interface
+    SpeechKITT.vroom();
+  }
 
