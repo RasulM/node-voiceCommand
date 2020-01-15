82560ecc55f9f2c1e7b41367aae8d5116c6e4974
Rasul Mahones <mahonesrasul@gmail.com>
2020-01-14 20:41:35 -0500

    Update clientside.js
    add defaults for  sendMethodToServer
    host and port
    
    added voice commands

--- Node_Tello_Drone_V2/public/new/clientside.js
+++ Node_Tello_Drone_V2/public/new/clientside.js
@@ -29,7 +29,7 @@ function bop(){
     sendMethodToServer(methodname,param,host)
     
 }
-function sendMethodToServer(methodname,param,host,port){
+function sendMethodToServer(methodname,param,host ="192.168.10.1",port=8889){
     var msg = {
         "method":{
             "name":"methodname",
@@ -87,7 +87,26 @@ function sendMethodToServer(methodname,param,host,port){
 }
 
 var voiceCommands ={
-    'hello': function() { alert('Hello world!'); }
+    'hello': function() { alert('Hello world!'); },
+    'start connection': function() {sendMethodToServer("create")},
+    'start command mode': function() {sendMethodToServer("commandmode")},
+    'start recording data': function() {sendMethodToServer('RecordData')},
+    'take off': function() {sendMethodToServer('takeOff')},
+    'land': function() {sendMethodToServer('land')},
+    'fly up :cm': function(cm) {sendMethodToServer('flyUp',cm+)},
+    'fly down :cm': function(cm) {sendMethodToServer('flyDown',cm)},
+    'fly left :cm': function(cm) {sendMethodToServer('flyLeft',cm)},
+    'fly right :cm': function(cm) {sendMethodToServer('flyRight',cm)},
+    'fly forward :cm': function(cm) {sendMethodToServer('flyForward',cm)},
+    'fly back :cm': function() {sendMethodToServer('flyBack')},
+    'rotate :degress': function(degress) {sendMethodToServer('rotate',degress)},
+    'flip :direction': function(direction) {sendMethodToServer('flipDrone',direction)},
+    'set speed to :speed': function(speed) {sendMethodToServer('setSpeed',speed)},
+    'close connection': function() {sendMethodToServer('closeConnections')},
+    'get current battery level': function() {sendMethodToServer('currentBattery')},
+    'get current speed': function() {sendMethodToServer('currentSpeed')},
+    'get current flight time': function() {sendMethodToServer('currentFlightTime')}
+   // '': function() {sendMethodToServer()}
   }
 
 if (annyang) {
